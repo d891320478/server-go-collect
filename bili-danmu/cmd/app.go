068665abefb2157e55dab8c7934c5503f39eb397
htdong <dht31261@gmail.com>
2023-11-21 10:08:42 +0800

    c

--- bili-danmu/cmd/app.go
+++ bili-danmu/cmd/app.go
@@ -4,12 +4,18 @@ import (
 	"context"
 	"fmt"
 
+	"dubbo.apache.org/dubbo-go/v3/config"
 	_ "dubbo.apache.org/dubbo-go/v3/imports"
 	"github.com/d891320478/server-go-collect/bili-danmu/bean"
 	"google.golang.org/protobuf/types/known/wrapperspb"
 )
 
 func main() {
+	config.SetConsumerService(bean.BiliRpcService)
+	if err := config.Load(); err != nil {
+		panic(err)
+	}
+
 	roomId := &wrapperspb.Int64Value{
 		Value: 222272,
 	}
