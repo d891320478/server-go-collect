195068120256ae67535884d8e56e2cd1ea066ecb
htdong <dht31261@gmail.com>
2023-11-21 21:26:30 +0800

    c

--- base-log/log_init.go
+++ base-log/log_init.go
@@ -6,6 +6,12 @@ import (
 	"github.com/jeanphorn/log4go"
 )
 
+var rotateSize = 100 * 1024 * 1024
+var format = "%D %T %L %S - %M"
+var maxBackup = 7
+var infoLog = "info"
+var errorLog = "error"
+
 func InitLog(appName string) {
 	// 创建日志目录
 	logPath := "/data/logs/" + appName
@@ -15,22 +21,22 @@ func InitLog(appName string) {
 	log4go.AddFilter("stdout", log4go.CRITICAL, log4go.NewConsoleLogWriter())
 	// info
 	infoLogWriter := log4go.NewFileLogWriter(logPath+"/info.log", true, true)
-	infoLogWriter.SetRotateSize(100 * 1024 * 1024)
-	infoLogWriter.SetRotateMaxBackup(7)
-	infoLogWriter.SetFormat("%D %T %L %S - %M")
-	log4go.AddFilter("info", log4go.INFO, infoLogWriter)
+	infoLogWriter.SetRotateSize(rotateSize)
+	infoLogWriter.SetRotateMaxBackup(maxBackup)
+	infoLogWriter.SetFormat(format)
+	log4go.AddFilter(infoLog, log4go.INFO, infoLogWriter)
 	// error
 	errorLogWriter := log4go.NewFileLogWriter(logPath+"/error.log", true, true)
-	errorLogWriter.SetRotateSize(100 * 1024 * 1024)
-	errorLogWriter.SetRotateMaxBackup(7)
-	errorLogWriter.SetFormat("%D %T %L %S - %M")
-	log4go.AddFilter("error", log4go.ERROR, errorLogWriter)
+	errorLogWriter.SetRotateSize(rotateSize)
+	errorLogWriter.SetRotateMaxBackup(maxBackup)
+	errorLogWriter.SetFormat(format)
+	log4go.AddFilter(errorLog, log4go.ERROR, errorLogWriter)
 }
 
 func InfoLog() *log4go.Filter {
-	return log4go.LOGGER("file")
+	return log4go.LOGGER(infoLog)
 }
 
 func ErrorLog() *log4go.Filter {
-	return log4go.LOGGER("error")
+	return log4go.LOGGER(errorLog)
 }
