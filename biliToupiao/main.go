a81e874bc6fde27ad8fe6f18b1c099d93e3179fe
htdong <dht31261@gmail.com>
2023-12-21 10:30:15 +0800

    c

--- biliToupiao/main.go
+++ biliToupiao/main.go
@@ -24,6 +24,7 @@ func Throwable() {
 	fmt.Println(string(debug.Stack()))
 }
 
+// CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o build/usr.exe -x src/Main.go
 func main() {
 	defer Throwable()
 	biliToupiao()
