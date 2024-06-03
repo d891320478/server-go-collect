a9ee10b70fa2e4888734d54c32087f373d116c6d
htdong <dht31261@gmail.com>
2024-06-03 17:56:44 +0800

    c

--- util/command/command.go
+++ util/command/command.go
@@ -1,18 +1,11 @@
 package command
 
 import (
-	"bytes"
 	"os/exec"
 )
 
 func RunCmd(cmd string) (string, error) {
-	buf := bytes.NewBuffer(nil)
 	cmd1 := exec.Command("/bin/bash", "-c", cmd)
-	cmd1.Stdout = buf
-	cmd1.Stderr = buf
-	err := cmd1.Run()
-	if err != nil {
-		return "", err
-	}
-	return buf.String(), err
+	output, err := cmd1.CombinedOutput()
+	return string(output), err
 }
