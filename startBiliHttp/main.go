afb0f0f0838c1554911c3f0b4673ffd57644c6c5
htdong <dht31261@gmail.com>
2025-03-29 21:19:26 +0800

    c

--- startBiliHttp/main.go
+++ startBiliHttp/main.go
@@ -1,8 +1,12 @@
 package main
 
-import "github.com/d891320478/server-go-collect/startBiliHttp/bililive"
+import (
+	baselog "github.com/d891320478/server-go-collect/base-log"
+	"github.com/d891320478/server-go-collect/startBiliHttp/bililive"
+)
 
 // CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o build/startBiliHttp -x main.go
 func main() {
+	baselog.InitLog("startBiliHttp")
 	bililive.StartBiliHttp()
 }
